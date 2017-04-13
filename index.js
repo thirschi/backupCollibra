fcf281ead6fd2e4221f36f20482468892076a4ca
thirschi <timothy_hirschi@byu.edu>
2017-04-13 11:49:00 -0600

    change to backup

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ exports.handler = function () {
     });
 
     var params = {
-        Bucket: 'ericn0temp',
-        Key: 'del2.txt'
+        Bucket: 'backupcollibra',
+        Key: 'BackupCollibra.json'
     }
     
     s3.getObject(params, function (err, data) {
@@ -15,7 +15,7 @@ exports.handler = function () {
         else {
 			console.log("Hello");
             var config = JSON.parse(data.Body.toString());
-            // console.log("This is the event=" + request.event);
+            console.log("This is the event=" + config);
         } // successful response
     });
 }
