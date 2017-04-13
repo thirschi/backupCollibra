92b5c75f1d08c0eada21e9c2715b0abc4565f30e
thirschi <timothy_hirschi@byu.edu>
2017-04-13 11:36:03 -0600

    try

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-var https = require('https');
 var AWS = require('aws-sdk');
 
-exports.handler = function (request) {
+exports.handler = function () {
     var s3 = new AWS.S3({
         region: 'us-west-2'
     });
 
     var params = {
-        Bucket: 'eventhub-to-sn',
-        Key: 'eventhub-config.json'
+        Bucket: 'ericn0temp',
+        Key: 'del2.txt'
     }
     
     s3.getObject(params, function (err, data, reqeust) {
         if (err) console.log(err, err.stack); // an error occurred
         else {
-            console.log("This is the request=" + request);
+			console.log("Hello");
             var config = JSON.parse(data.Body.toString());
-             console.log("This is the event=" + request.event);
+            // console.log("This is the event=" + request.event);
         } // successful response
     });
 }
