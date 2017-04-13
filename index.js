27cd215cb9b5388783c4b00c60eaf1c13040d4b9
thirschi <timothy_hirschi@byu.edu>
2017-04-13 11:19:52 -0600

    trying this

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var https = require('https');
 var AWS = require('aws-sdk');
-var path = require('path');
-exports.handler = function () {
-	var s3 = new AWS.S3({
-		region: 'us-west-2'
-	});
-	var params = {
-		Bucket: 'backupcollibra'
-		, Key: 'BackupCollibra.json'
-	}
-	s3.getObject(params, function (err, data) {
-		if (err) console.log(err, err.stack); // an error occurred
-		else {
-			console.log(data.Body.toString());
-			var config = JSON.parse(data.Body.toString());
-		} // successful response
-	});
+
+exports.handler = function (request) {
+    var s3 = new AWS.S3({
+        region: 'us-west-2'
+    });
+
+    var params = {
+        Bucket: 'eventhub-to-sn',
+        Key: 'eventhub-config.json'
+    }
+    
+    s3.getObject(params, function (err, data, reqeust) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            console.log("This is the request=" + request);
+            var config = JSON.parse(data.Body.toString());
+             console.log("This is the event=" + request.event);
+        } // successful response
+    });
 }
